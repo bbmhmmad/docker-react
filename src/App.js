2da82a55c2c7936e008101201d4a4d24df0c1378
Basim Muhammad <bbmhmmad@Basims-MacBook-Pro.local>
2019-02-18 19:50:55 -0600

    initial commit

--- src/App.js
+++ src/App.js
@@ -17,7 +17,7 @@ class App extends Component {
             target="_blank"
             rel="noopener noreferrer"
           >
-            Learn React
+            Learn React. It's so cool man!
           </a>
         </header>
       </div>
